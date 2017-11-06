9267f7762156e803571f655a6eaffc2820abe470
Simbrams <simon.brami@epitech.eu>
2017-11-06 15:08:52 +0100

    IOMessages

---
+++ cardGames/Common/srcs/IOMessages/IOMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.IOMessages
+{
+    public static class IOMessages
+    {
+        public static string success = "Success!";
+        public static string error = "Error!";
+    }
+}
