efc5e5d6c7be9981b02f6e9c4f88ac888934c8fa
Axel Drozdzynski <axel1.drozdzynski@epitech.eu>
2017-11-06 15:57:23 +0100

    Packets Namespace

---
+++ cardGames/Common/srcs/NetworkUtils/Packets/Packets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtils
+{
+    namespace Packets
+    {
+        public class Packet00Message
+        {
+            public int 
+        };
+    }
+}
