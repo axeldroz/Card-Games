61a9b0ff5d7bb6a2a81821977cb1b4a605f0c9b8
Axel Drozdzynski <axeldroz@hotmail.fr>
2017-11-06 19:24:50 +0100

    Add Packets structures

---
+++ cardGames/Common/srcs/NetworkUtils/Packets.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtils
+{
+    namespace Packets
+    {
+        /// <summary>
+        /// Packet00Message : String message
+        /// </summary>
+        public struct Packet00Message
+        {
+            string message;
+        };
+
+        /// <summary>
+        /// Packet01LoginRequest : String name
+        /// </summary>
+        public struct Packet01LoginRequest
+        {
+            string name;
+        }
+
+        /// <summary>
+        /// Packet02LoginAnswer : bool accepted
+        /// </summary>
+        public struct Packet02LoginAnswer
+        {
+            bool accepted;
+        }
+
+        /// <summary>
+        /// Packet03WaitGameRequest : Nothing
+        /// </summary>
+        public struct Packet03WaitGameRequest
+        {
+            
+        }
+
+        /// <summary>
+        /// Packet04WaitGameAnswer : bool accepted
+        /// </summary>
+        public struct Packet04WaitGameAnswer
+        {
+            bool accepted;   
+        }
+
+        /// <summary>
+        /// Packet05BetRequest : string descr, string type, Bet last_bet
+        /// </summary>
+        public struct Packet05BetRequest
+        {
+            string descr;
+            string type;
+            //Bet last_bet;
+        }
+
+        /// <summary>
+        ///  Packet06BetAnswer : string type, Bet newbet
+        /// </summary>
+        public struct Packet06BetAnswer
+        {
+            string type;
+            //Bet newbet
+        }
+
+    }
+}
