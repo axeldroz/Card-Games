f874972682d504c94aae1559c5cbdcaff7d122d8
Simbrams <simon.brami@epitech.eu>
2017-11-07 20:27:30 +0100

    stuffs

--- cardGames/CoincheServer/srcs/Game/GameManagerServer.cs
+++ cardGames/CoincheServer/srcs/Game/GameManagerServer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.GameUtils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,72 @@ namespace Game
 {
     public class GameManagerServer
     {
+        protected List<Player> player;
+        protected Deck deck;
+        protected List<Team> team;
+        protected Bet bet;
+        protected Deck handStack;
+        protected bool isReady;
+        protected CoincheServer.NetworkServer.CoincheServer server;
+
         public GameManagerServer()
         {
+            player = new List<Player>();
+            deck = new Deck();
+            team = new List<Team>();
+            bet = new Bet();
+            handStack = new Deck();
+        }
 
+        public List<Player> Player
+        {
+            get { return player; }
+            set { player = value; }
+        }
+
+        public Deck Deck
+        {
+            get { return deck; }
+            set { deck = value; }
+        }
+
+        public List<Team> Team
+        {
+            get { return team; }
+            set { team = value; }
+        }
+
+        public Bet Bet
+        {
+            get { return bet; }
+            set { bet = value; }
+        }
+
+        public Deck HandStack
+        {
+            get { return handStack; }
+            set { handStack = value; }
+        }
+
+        public bool IsReady
+        {
+            get { return isReady; }
+            set { isReady = value; }
+        }
+
+        public Player getPlayer(Player p)
+        {
+            foreach (Player elem in player)
+            {
+                if (elem.Id == p.Id)
+                    return (elem);
+            }
+            return (null);
+        }
+
+        public void startHand()
+        {
+            Console.WriteLine("Game Starts!");
         }
     }
 }
--- cardGames/CoincheServer/srcs/Game/TableManager.cs
+++ cardGames/CoincheServer/srcs/Game/TableManager.cs
@@ -1,16 +1,60 @@
-﻿using System;
+﻿using Common.GameUtils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoincheServer;
+using System.IO;
 
 namespace Game
 {
-    public class TableManager
+    public class TableManager : GameManagerServer
     {
         public TableManager()
         {
-
+            
         }
+
+        public Team findTeamAvailable()
+        {
+            foreach (Team elem in team)
+            {
+                if (!elem.full)
+                    return (elem);
+            }
+            return (null);
+        }
+
+        public int addPlayer(string name, int id)
+        {
+            try
+            {
+                if (name == null || id < 0)
+                    throw new ArgumentException("Error: The player must have at least a name and an id >= 0");
+                Team tmp;
+                if ((tmp = findTeamAvailable()) == null)
+                    throw new Exception("Error: All teams are full");
+                Player newPlayer = new Player
+                {
+                    Name = name,
+                    Id = id,
+                    Team = tmp
+                };
+                player.Add(newPlayer);
+                Console.WriteLine("Player " + name + " added to the team N°" + tmp.Id);
+                if (tmp.Player.Count == 2)
+                    tmp.Full = true;
+                return (0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " // Source : " + ex.Source);
+                return (-1);
+            }
+            
+        }
+        
+
     }
 }
