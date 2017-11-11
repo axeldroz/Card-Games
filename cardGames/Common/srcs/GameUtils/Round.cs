a9769850cb874b8b45711d6a07ec0328a6834aa5
Simbrams <simon.brami@epitech.eu>
2017-11-11 17:56:55 +0100

    modifsRound

--- cardGames/Common/srcs/GameUtils/Round.cs
+++ cardGames/Common/srcs/GameUtils/Round.cs
@@ -52,7 +52,7 @@ namespace Common.GameUtils
                 if (cards.Count > 0)
                 {
                     cards.RemoveRange(0, cards.Count);
-                    IO.OutputManager.Debug.Display("/!\\ Cards reset for new round/plit.");
+                    IO.OutputManager.Debug.Display("/!\\ Cards reset for new round/plis.");
                 }
             }
             catch (Exception ex)
@@ -64,14 +64,16 @@ namespace Common.GameUtils
         /// <summary>
         /// Check wich team Won the plit and distribute point to players and Teams.
         /// </summary>
-        public void WhoWonRound()
+        public Player WhoWonRound()
         {
-            if (cards.Count == 8 && !Asset.Equals(null))
-            {
-                int ScoreTeamRed = 0;
-                int ScoreTeamBlue = 0;
+            int ScoreTeamRed = 0;
+            int ScoreTeamBlue = 0;
+            Player SaveBest = new Player();
+            int lastCard = 0;
 
-                try
+            try
+            {
+                if (cards.Count == 8 && !Asset.Equals(null))
                 {
                     // Check all cards of the plit.
                     foreach (var elem in cards)
@@ -83,6 +85,11 @@ namespace Common.GameUtils
                             elem.player.Team.Score += elem.pointA;
                             ScoreTeamRed += elem.player.Team.Name == "Red" ? elem.pointA : 0;
                             ScoreTeamBlue += elem.player.Team.Name == "Blue" ? elem.pointA : 0;
+                            if (lastCard < elem.pointA)
+                            {
+                                SaveBest = elem.player;
+                                lastCard = elem.pointA;
+                            }
                         }
                         else // Else the card is not an asset so it takes usual points.
                         {
@@ -90,23 +97,32 @@ namespace Common.GameUtils
                             elem.player.Team.Score += elem.point;
                             ScoreTeamRed += elem.player.Team.Name == "Red" ? elem.point : 0;
                             ScoreTeamBlue += elem.player.Team.Name == "Blue" ? elem.point : 0;
+                            if (lastCard < elem.point)
+                            {
+                                SaveBest = elem.player;
+                                lastCard = elem.point;
+                            }
+
                         }
                     }
+                    IO.OutputManager.Debug.Display((ScoreTeamRed > ScoreTeamBlue) ? "Red Team Won the plis" : "Blue Team Won the plis");
+                    // Put action to do after end of plit. ClearTable() for example ?
+                    return SaveBest;
                 }
-                catch (ArgumentNullException ex)
-                {
-                    throw ex;
-                }
-                IO.OutputManager.Debug.Display((ScoreTeamRed > ScoreTeamBlue) ? "Red Team Won the plit" : "Blue Team Won the plit");
-                // Put action to do after end of plit. ClearTable() for example ?
+                return null;
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
             }
         }
 
         /// <summary>
         /// Check who won the Bet and give points to the winner and his team.
         /// </summary>
-        public void WhoWonBet()
+        public Player WhoWonBet()
         {
+            Player save = new Player();
             try
             {
                 foreach (var elem in cards)
@@ -114,11 +130,14 @@ namespace Common.GameUtils
                     if (elem.suit == Asset.suit && elem.number == Asset.number)
                     {
                         // Je donne au joueur et à son équipe le nombre de point que pèse le Bet (JE SUIS PAS SUR DE LA REGLE à VERIFIER)
+                        save = elem.player;
                         elem.player.Score += CurrentBet.points;
                         elem.player.Team.Score += CurrentBet.points;
                         IO.OutputManager.Debug.Display("Player " + elem.player.Name + " of Team " + elem.player.Team.Name + " won the bet of " + CurrentBet.points + " points !!");
+                        return save;
                     }
                 }
+                return null;
             }
             catch (ArgumentNullException ex)
             {
